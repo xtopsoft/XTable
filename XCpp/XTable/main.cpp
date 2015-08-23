030602d75b7a9c0834616641de8c0ae21e8ccdc0
dad <dad@daddeMac-mini.local>
2015-08-23 23:27:45 +0800

    gtest

--- XCpp/XTable/main.cpp
+++ XCpp/XTable/main.cpp
@@ -1,8 +1,6 @@
-#include <iostream>
+#include "gtest/gtest.h"
 
-using namespace std;
-
-int main() {
-    cout << "Hello, World!" << endl;
-    return 0;
+int main(int argc, char* argv[]) {
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
